8245fa8588f04558f560c027ba4e56d9e488d12c
Dukurma <barryyabilsu@gmail.com>
2023-03-28 19:19:01 +0000

    pYTH0# Cl@$$&$

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """Define a class Square."""
 
 
@@ -12,4 +11,4 @@ class Square:
         Args:
             size (int): The size of the new square.
         """
-        self.__size = size	
+        self.__size = size
