cbd903d0d438a5a720488f27833c73f390fb3a9d
Dukurma <barryyabilsu@gmail.com>
2023-06-18 10:57:24 -0700

    python_orm

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
+"""
+the class definition of City
+"""
 
-from sqlalchemy import Column, Integer, String, ForeignKey
+import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
-
-""" A python file that contains class definition of a City
-    and an instance Base = declarative_base():"""
-
+from sqlalchemy import Column, Integer, String, ForeignKey
 Base = declarative_base()
 
 
 class City(Base):
-    """ Defines the City class """
+    """Representation of city class"""
 
-    __tablename__ = "cities"
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    __tablename__ = 'cities'
+    id = Column(Integer, autoincrement=True, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    state_id = Column(Integer, ForeignKey('id'), nullable=False)
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
+"""
+the class definition of a State
+"""
 
+
+import sqlalchemy
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-
-""" A python file that contains class definition of a State
-    and an instance Base = declarative_base():"""
 
 Base = declarative_base()
 
 
 class State(Base):
-    """ Defines the State class """
-
-    __tablename__ = "states"
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    """Representation of a state"""
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement=True, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
